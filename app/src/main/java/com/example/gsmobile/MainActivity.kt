9305598872c6bbb0ef4797255f4313987488a91f
caverzikA <caverzang@gmail.com>
2024-06-14 19:55:49 -0300

    commit2

--- app/src/main/java/com/example/gsmobile/MainActivity.kt
+++ app/src/main/java/com/example/gsmobile/MainActivity.kt
@@ -10,10 +10,11 @@ import com.example.gsmobile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
         NavigationUI.setupActionBarWithNavController(this, navController)
